2309be8a0f24b423bfcd5fc1d66911fc049c46b6
Krunal Thakar <kru.dipnil@gmail.com>
2025-05-18 20:32:40 -0400

    Add SQL query files for selecting cities excluding specific vowels

---
+++ SQL (Basic)/Basic Select/Weather Observation Station 12.sql
@@ -0,0 +1,15 @@
+/*
+Enter your query here.
+*/
+SELECT DISTINCT city
+FROM station
+WHERE UPPER(city) NOT LIKE '%A'
+  AND UPPER(city) NOT LIKE '%E'
+  AND UPPER(city) NOT LIKE '%I'
+  AND UPPER(city) NOT LIKE '%O'
+  AND UPPER(city) NOT LIKE '%U'
+  AND UPPER(city) NOT LIKE 'A%'
+  AND UPPER(city) NOT LIKE 'E%'
+  AND UPPER(city) NOT LIKE 'I%'
+  AND UPPER(city) NOT LIKE 'O%'
+  AND UPPER(city) NOT LIKE 'U%';
